995ed0c64a9e8200b383636bdd59e027ee57fa50
Yevhen R <7374618+yevhen-romanenko@users.noreply.github.com>
2020-03-21 15:16:02 +0200

    Updates

---
+++ precache-manifest.6a3a8211a1ddadfda29f2aab169b4839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0288c797fedb868c91310913ed5bfe1d",
+    "url": "/movie-list/index.html"
+  },
+  {
+    "revision": "ed0fa78a2e41d8d7b634",
+    "url": "/movie-list/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ed0fa78a2e41d8d7b634",
+    "url": "/movie-list/static/js/2.f2a2bf39.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie-list/static/js/2.f2a2bf39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc63f56b63d9de949637",
+    "url": "/movie-list/static/js/main.89aa0332.chunk.js"
+  },
+  {
+    "revision": "75300ad61a2e1fe3cc2b",
+    "url": "/movie-list/static/js/runtime-main.f5676573.js"
+  }
+]);
